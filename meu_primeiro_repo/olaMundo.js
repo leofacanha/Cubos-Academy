923572d5293111f25c3465e58dc1ca368aa476a4
Your Name <leofacanhadev@gmail.com>
2023-07-04 15:13:19 -0300

    criado let cidade

--- meu_primeiro_repo/olaMundo.js
+++ meu_primeiro_repo/olaMundo.js
@@ -2,4 +2,5 @@ let nome = ("Leonardo de Sousa Façanha");
 let rosto = ("Lindo");
 let cheiro = ("Cheiroso");
 let idade = (36);
-console.log(nome, "é", rosto, "e", cheiro, "tem", idade, "anos",);
+let cidade = ("Fortaleza")
+console.log(nome, "é", rosto, "e", cheiro, "tem", idade, "anos", "Que mora na Cidade de:", cidade,);
